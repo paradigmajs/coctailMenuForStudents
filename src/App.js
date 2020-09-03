9a5d02228585f698aa15c8ee1b8fecd03cc86f6f
paradigmajs <bakhautdinov.adam@gmail.com>
2020-09-03 13:10:52 +0600

    created hooks

--- src/App.js
+++ src/App.js
@@ -1,55 +1,70 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect} from 'react'
 import All from './All'
-export default class App extends Component {
+
+function App  () {
+
+  const [data,setData] = useState(null)
+
+  useEffect(() => {
+    go()
+  }, [])
   
-  state={
-    data:[]
-  }
-  componentDidMount (){
-    this.go()
-  }
-  go = async ()=>{
+  const go = async ()=>{
     let resp = await fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail')
     let data = await resp.json()
-
-    this.setState({
-      data:data.drinks
-    })
-
+    setData(data.drinks)
   }
-  search=(val)=>{
+  const search=(val)=>{
     let name = val
     let url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a='
     if(name=== 'Non_Alcoholic'){
-      this.find(name, url)
+      find(name, url)
     }else if(name === 'Alcoholic'){
-      this.find(name, url)
+      find(name, url)
     }else{
-      this.go()
+      go()
+    }
+  }
+
+  const find = async (name, url)=>{
+    let resp  = await fetch (url+name)
+    let data = await resp.json()
+    setData(data.drinks)
+  }
+
+  const filterByName= async (val)=>{
+    if(val !== ''){
+      let resp = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+val)
+      let data = await resp.json()
+      setData(data.drinks)
+    }
+    else{
+      go()
     }
 
   }
-
-  find = async (name, url)=>{
-    let resp  = await fetch (url+name)
-    let data = await resp.json()
-    this.setState({
-      data:data.drinks
-    })
-  }
-  render() {
     return (
+     
       <div>
         <h1>Coctails</h1>
+
+        <input 
+            placeholder="search coctail by name"
+            type="text"
+            onChange={(e)=>{
+              filterByName(e.target.value)
+            }}
+         />
+
         <select onChange={(e)=>{
-          this.search(e.target.value)
+          search(e.target.value)
         }}>
           <option value="Coctail">ALL</option>
           <option value="Non_Alcoholic">NON-ALC</option>
           <option value="Alcoholic">ALC</option>
         </select>
-        {this.state ? 
-          this.state.data.map(elem=>{
+        {data ? 
+          data.map(elem=>{
             return (
               <All coctail = {elem}/>
             )
@@ -57,5 +72,6 @@ export default class App extends Component {
         }
       </div>
     )
-  }
+  
 }
+export default App
